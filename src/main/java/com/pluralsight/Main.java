59cc156ada3c2368fbbe672f2f509ccbd1a0d667
mattchristenson <matt@rcc.team>
2026-04-22 13:26:58 -0500

    made file manager using oop patterns

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,14 +1,20 @@
+package com.pluralsight;
+
 import com.pluralsight.Product;
-import com.pluralsight.data.ProductLoader;
-import com.pluralsight.data.ProductWriter;
+import com.pluralsight.data.ProductFileManager;
 import com.pluralsight.ui.Console;
 
 import java.io.*;
+import java.util.ArrayList;
 
 import static com.pluralsight.ui.Console.*;
 
-public static void main(String[] arg) throws IOException {
-        ArrayList<Product> products = ProductLoader.loadProduct();
+public class Main{
+
+    private final static ProductFileManager productFileManager = new ProductFileManager("src/main/java/com/pluralsight/data/product.txt");
+
+    public static void main(String[] arg) throws IOException {
+        ArrayList<Product> products = productFileManager.loadProduct();
 
         String productSystemMenuMsg = """
             What do you want to do?
@@ -55,71 +61,73 @@ public static void main(String[] arg) throws IOException {
 
 //   Product Menu System
 
-public static int productMenuSystem() {
-    return Console.askForInt("Please enter a command:");
+    public static int productMenuSystem() {
+        return Console.askForInt("Please enter a command:");
 
-}
-
-public static void listAllProduct(ArrayList<Product> products){
-    displayProduct(products);
-
-}
-
-public static void displayProduct(ArrayList<Product> products){
-    for(int i = 0; i < products.size(); i++){
-        Product p = products.get(i);
-        System.out.printf("%d %s $%.2f\n", p.getProductId(), p.getProductName(), p.getProductPrice());
     }
-}
 
-public static void findProductById(ArrayList<Product> products){
-    int askId = Console.askForInt("What is the product id?");
+    public static void listAllProduct(ArrayList<Product> products){
+        displayProduct(products);
 
-    for (Product p : products) {
-        if (p.getProductId() == askId) {
-            System.out.printf("Found: [ID: %d] [PRODUCT: %s] [RETAIL PRICE: $%.2f]\n", p.getProductId(), p.getProductName(), p.getProductPrice());
-            return;
+    }
+
+    public static void displayProduct(ArrayList<Product> products){
+        for(int i = 0; i < products.size(); i++){
+            Product p = products.get(i);
+            System.out.printf("%d %s $%.2f\n", p.getProductId(), p.getProductName(), p.getProductPrice());
+        }
+    }
+
+    public static void findProductById(ArrayList<Product> products){
+        int askId = Console.askForInt("What is the product id?");
+
+        for (Product p : products) {
+            if (p.getProductId() == askId) {
+                System.out.printf("Found: [ID: %d] [PRODUCT: %s] [RETAIL PRICE: $%.2f]\n", p.getProductId(), p.getProductName(), p.getProductPrice());
+                return;
+            }
+
+        }
+        System.out.printf("Cannot find a product with this id. \n");
+
+    }
+
+    public static ArrayList<Product> findProductByWithinPriceRange(ArrayList<Product> products) {
+        double askMinPrice = askForDouble("What's the minimum price");
+        double askMaxPrice = askForDouble("What's the maximum price");
+
+        ArrayList<Product> matchingItems = new ArrayList<>();
+
+        for(Product p : products) {
+
+            if (askMaxPrice >= (p.getProductPrice()) && askMinPrice <= (p.getProductPrice())){
+                matchingItems.add(p);
+            }
+
         }
 
-    }
-    System.out.printf("Cannot find a product with this id. \n");
-
-}
-
-public static ArrayList<Product> findProductByWithinPriceRange(ArrayList<Product> products) {
-    double askMinPrice = askForDouble("What's the minimum price");
-    double askMaxPrice = askForDouble("What's the maximum price");
-
-    ArrayList<Product> matchingItems = new ArrayList<>();
-
-    for(Product p : products) {
-
-        if (askMaxPrice >= (p.getProductPrice()) && askMinPrice <= (p.getProductPrice())){
-            matchingItems.add(p);
+        if(matchingItems.isEmpty()) {
+            System.out.printf("No item found within your price range");
         }
-
+        System.out.println("Found " + matchingItems.size() + (matchingItems.size() == 1 ? " result." : " results." ));
+        displayProduct(matchingItems);
+        return matchingItems;
     }
 
-    if(matchingItems.isEmpty()) {
-        System.out.printf("No item found within your price range");
-    }
-    System.out.println("Found " + matchingItems.size() + (matchingItems.size() == 1 ? " result." : " results." ));
-    displayProduct(matchingItems);
-    return matchingItems;
-}
+    public static void addNewProduct(ArrayList<Product> products) {
+        int addNewProductId = askForInt("What's the product id: ");
+        String addNewProductName = askForString("What's the product name: ");
+        double addNewProductPrice = askForDouble("What's the price of this product: ");
+        Product addNewProduct = new Product(addNewProductId, addNewProductName, addNewProductPrice);
+        products.add(addNewProduct);
+        try {
+            productFileManager.writeProduct(addNewProduct);
+        } catch (IOException e){
+            System.out.println("An error occured while trying to update the product dateset." + e.getMessage() + e.getCause());
 
-public static void addNewProduct(ArrayList<Product> products) {
-    int addNewProductId = askForInt("What's the product id: ");
-    String addNewProductName = askForString("What's the product name: ");
-    double addNewProductPrice = askForDouble("What's the price of this product: ");
-    Product addNewProduct = new Product(addNewProductId, addNewProductName, addNewProductPrice);
-    products.add(addNewProduct);
-    try {
-        ProductWriter.writeProduct(addNewProduct);
-    } catch (IOException e){
-        System.out.println("An error occured while trying to update the product dateset." + e.getMessage() + e.getCause());
+        }
+        System.out.println("Product was added to the dateset.");
 
     }
-    System.out.println("Product was added to the dateset.");
 
 }
